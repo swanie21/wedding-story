5eefd78555cd07643b42024d1059d41834227144
Kirsten Swanson <kirsten.swanson@brandfolder.com>
2021-03-23 21:57:19 -0600

    use hook to add script in body

--- pages/adventure-fund/index.tsx
+++ pages/adventure-fund/index.tsx
@@ -1,14 +1,15 @@
+import { useScript } from '../../hooks';
+
 import { Meta } from '../../components/Meta';
 import { Nav } from '../../components/Nav';
 
 const AdventureFund: React.FunctionComponent = () => {
+  useScript('//www.myregistry.com//Visitors/GiftList/iFrames/EmbedRegistry.ashx?r=oXEZw-KMmQYfSDjRFZPA7g2&v=2', 'script_myregistry_giftlist_iframe');
+
   return (
     <>
       <Meta canonical="/adventure-fund" />
       <Nav />
-      <main>
-        <br /><script id='script_myregistry_giftlist_iframe' type='text/javascript' src='//www.myregistry.com//Visitors/GiftList/iFrames/EmbedRegistry.ashx?r=oXEZw-KMmQYfSDjRFZPA7g2&v=2'></script>
-      </main>
     </>
   );
 };
