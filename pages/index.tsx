001a6b8045329c3393cb341e37a85d130fa32273
Kirsten Swanson <kirsten.swanson@brandfolder.com>
2021-03-31 23:28:06 -0600

    add react confetti library

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import React from 'react'
+import Confetti from 'react-confetti';
 import { Meta } from '../components/Meta';
 import { Nav } from '../components/Nav';
 import styles from '../styles/Home.module.scss';
@@ -9,6 +11,9 @@ const Home: React.FunctionComponent = () => {
       <div className={styles.container}>
         <Nav />
         <main className={styles.home}>
+          <Confetti
+            colors={['#7400B8','#6930C3','#5E60CE','#5390D9','#4EA8DE','#56CFE1','#64DFDF','#72EFDD','#80FFDB']}
+          />
           <h1 className={styles.title}>Drew & Kirsten</h1>
           <h2>Saturday, August 7th 2021</h2>
           <h2>6 o'clock evening ceremony with reception to follow</h2>
