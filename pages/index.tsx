afa66b835476b701c456bd817a877be0838fe98f
Kirsten Swanson <kirsten.swanson@brandfolder.com>
2021-04-05 20:32:12 -0600

    get window size on page load for confetti

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Confetti from 'react-confetti';
 import { Meta } from '../components/Meta';
 import { Nav } from '../components/Nav';
 import styles from '../styles/Home.module.scss';
 
+const getWindowSize = () => {
+  const [windowSize, setWindowSize] = useState({
+    height: undefined,
+    width: undefined,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({
+        height: window.innerHeight,
+        width: window.innerWidth,
+      });
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+  return windowSize;
+}
+
 const Home: React.FunctionComponent = () => {
+  const size = getWindowSize();
+
   return (
     <>
       <Meta canonical="/home" />
@@ -13,8 +38,8 @@ const Home: React.FunctionComponent = () => {
         <main className={styles.home}>
           <Confetti
             colors={['#7400B8','#6930C3','#5E60CE','#5390D9','#4EA8DE','#56CFE1','#64DFDF','#72EFDD','#80FFDB']}
-            height={1000}
-            width={1680}
+            height={size.height}
+            width={size.width}
           />
           <h1 className={styles.title}>Drew & Kirsten</h1>
           <h2>Saturday, August 7th 2021</h2>
